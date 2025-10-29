ca326851a3ecb8e63f9cd2b9cf6f5186bfe708ad
EzzEdineMohamed <eemmahamed1974@gmail.com>
2025-10-29 21:13:17 +0300

    fix: corrected footer import path case

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
-import Footer from "../components/footer/footer";
+import Navbar from "../components/navbar/Navbar.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
